d6715acc8eb20b998595a6a389f5cf229418c3da
geometriahater <m.v.dyomin@mail.ru>
2020-07-01 12:27:47 +0300

    moved to maven

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
+fun toJSON(collection: Collection<Int>): String {
+  val builder = StringBuilder("[")
+  val iterator: Iterator<Int> =collection.iterator()
+  while (iterator.hasNext()){
+    builder.append(iterator.next())
+    if (iterator.hasNext()) builder.append(", ")
+  }
+  builder.append("]")
+  return builder.toString()
+}
+
 fun main(args: Array<String>) {
-	println("Hello world")
+  println("Hello world")
 }
