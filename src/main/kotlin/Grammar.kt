c6df89582292ba732c39c6115c556356064444f8
geometriahater <m.v.dyomin@mail.ru>
2020-07-02 15:58:02 +0300

    что-то происходит

--- src/main/kotlin/Grammar.kt
+++ src/main/kotlin/Grammar.kt
@@ -3,32 +3,32 @@ interface Expression
 interface Symbol : Expression
 
 data class NonTerminal(val name: String): Symbol{
-	override fun toString(): String = name
+  override fun toString(): String = name
 }
 
 data class Terminal(val value: String): Symbol{
-	override fun toString(): String = "\'"+ value.replace("\'", "\\\'") + "\'"
+  override fun toString(): String = "\'"+ value.replace("\'", "\\\'") + "\'"
 }
 
 data class Sequence(val parts: List<Expression>): Expression{
-	override fun toString(): String = parts.fold("", {acc, e -> acc + " " + e})
-	constructor (vararg exprs: Expression) : this(exprs.toList())
+  constructor(vararg exprs: Expression) : this(exprs.toList())
+  override fun toString(): String = parts.fold("", {acc, e -> acc + " " + e})
 }
 
-data class Choise(val variants: Set<Expression>): Expression{
-	override fun toString(): String = "(" + variants.fold("", {acc, e -> acc + " | " + e}) + ")"
-	constructor (vararg exprs: Expression) : this(exprs.toSet())
+data class Choise(val variants: List<Expression>): Expression{
+  constructor(vararg exprs: Expression) : this(exprs.toList())
+  override fun toString(): String = variants.fold("( ", {acc, e -> acc + e + " | "}).substringBeforeLast("|") + ")"
 }
 
 data class Repeat(val repeatable: Expression): Expression{
-	override fun toString(): String = "{ " + repeatable + " }"
+  override fun toString(): String = "{ " + repeatable + " }"
 }
 
-data class Maybe(val possible: Expression): Expression{
-	override fun toString(): String = "[ " + possible + " ]"
+data class Maybe(val possible: Expression): Expression{  
+  override fun toString(): String = "[ " + possible + " ]"
 }
 
-data class Grammar(val grammar: Map<NonTerminal, Expression>){
-	override fun toString(): String = grammar.entries.fold("", {acc, entry -> acc + entry.key + " = " + entry.value + "\n"})
-	constructor (vararg pairs: Pair<NonTerminal, Expression>) : this(pairs.associateBy({it.first}, {it.second})) 
+data class Grammar(val rules: Map<NonTerminal, Expression>){
+  constructor(vararg pairs: Pair<NonTerminal, Expression>) : this(pairs.associateBy({it.first}, {it.second})) 
+  override fun toString(): String = rules.entries.fold("", {acc, entry -> acc + entry.key + " = " + entry.value + "\n"})
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,22 @@
-fun main(args: Array<String>){
-	//val grammar = Grammar(NonTerminal("a") to Choise(Terminal("b"), Terminal("c")))
-	//println(grammar)
-	println("a")
+fun main(args: Array<String>) {
+  val grammar = Grammar(
+    NonTerminal("a") to Choise(NonTerminal("a"), Terminal("b"), Repeat(Terminal("B"))),
+    NonTerminal("b") to Choise(NonTerminal("b"), Terminal("b"), Repeat(Terminal("B"))),
+    NonTerminal("c") to Choise(NonTerminal("c"), Terminal("b"), Repeat(Terminal("B")))
+  )
+  println(TextGenerator(grammar, 4).generateText(Sequence(NonTerminal("a"), NonTerminal("b"), NonTerminal("c"))))
 }
 
+class TextGenerator(val grammar: Grammar, val repeatBound: Int) {
+  private val random = java.util.Random()
+
+  fun generateText(start: Expression): String = when (start) {
+    is Terminal -> start.value
+    is NonTerminal -> generateText(grammar.rules.get(start)!!)
+    is Maybe -> if (random.nextBoolean()) generateText(start.possible) else ""
+    is Sequence -> start.parts.fold("", {acc, e -> acc + generateText(e)})
+    is Repeat -> Array(random.nextInt(repeatBound), {generateText(start.repeatable)}).fold("", String::plus)
+    is Choise -> generateText(start.variants.get(random.nextInt(start.variants.size)))
+    else -> "" 
+  }
+}
