510b4924444fe377d55c98178e7e4d644d649a24
geometriahater <m.v.dyomin@mail.ru>
2020-07-06 00:11:46 +0300

    теперь грамматика упрощается!

--- src/main/kotlin/grammar/SimplifiedGrammar.kt
+++ src/main/kotlin/grammar/SimplifiedGrammar.kt
@@ -1,17 +1,74 @@
 package grammar
 
 data class SimplifiedRule(val symbols: List<Symbol>) {
-  override fun toString(): String = symbols.fold("", { acc, e -> acc + " " + e })
+  override fun toString(): String = 
+    if (symbols.size> 0) symbols.fold("", { acc, e -> acc + " " + e }) else "_"
   constructor(vararg syms: Symbol) : this(syms.toList())
 }
 
+typealias RulesMap = HashMap<NonTerminal, ArrayList<SimplifiedRule>>
+
 data class SimplifiedGrammar(val rules: Map<NonTerminal, List<SimplifiedRule>>) {
   constructor(vararg theRules: Pair<NonTerminal, List<SimplifiedRule>>) : this(theRules.associateBy({ it.first }, { it.second }))
-  override fun toString(): String = rules.entries.fold("", { acc, entry -> "" + acc + entry.key + " = " + entry.value.fold("", { a, b -> a + "\n\t" + b }) })
+  override fun toString(): String = rules.entries.fold("", { acc, entry -> "" + acc + "\n" + entry.key + " = " + entry.value.fold("", { a, b -> a + "\n\t" + b }) })
   public fun toGrammar(): Grammar = Grammar(rules.mapValues { entry -> Choise(entry.value.map { Sequence(it.symbols) }) })
-  
+
   companion object {
-    public fun fromGrammar(grammar: Grammar) : SimplifiedGrammar = TODO("запилить таки упрощалку")
+    private fun getList(nonTerminal: NonTerminal, targetRules: RulesMap): ArrayList<SimplifiedRule> =
+        targetRules.get(nonTerminal) ?: {
+        val newList = ArrayList<SimplifiedRule>()
+        targetRules.put(nonTerminal, newList)
+        newList
+        }()
+
+    private fun addRule(rule: SimplifiedRule, nonTerminal: NonTerminal, targetRules: RulesMap) =
+      getList(nonTerminal, targetRules).add(rule)
+
+    private fun processRule(nonTerminal: NonTerminal, rule: Expression, targetRules: RulesMap) {
+      when (rule) {
+        is Symbol -> addRule(SimplifiedRule(rule), nonTerminal, targetRules)
+        is Choise ->{
+          var i =0 
+          for (variant in rule.variants){
+            i+=1
+            val newNonTerminal = NonTerminal(nonTerminal.name+"_"+ i)
+            processRule(newNonTerminal, variant, targetRules)
+            addRule(SimplifiedRule(newNonTerminal), nonTerminal, targetRules)
+          }
+        }
+        is Repeat -> {
+          processRule(nonTerminal, Sequence(rule.repeatable, nonTerminal), targetRules)
+          addRule(SimplifiedRule(), nonTerminal, targetRules)
+        }
+        is Maybe -> {
+          processRule(nonTerminal, rule.possible, targetRules)
+          addRule(SimplifiedRule(), nonTerminal, targetRules)
+        }
+        is Sequence -> {
+          val newRuleList: MutableList<Symbol> = ArrayList<Symbol>()
+          var i = 0
+          for (expr in rule.parts) {
+            i += 1
+            when (expr) {
+              is Symbol -> newRuleList.add(expr)
+              else -> {
+                val newNonTerminal = NonTerminal(nonTerminal.name + "_" + i)
+                processRule(newNonTerminal, expr, targetRules)
+                newRuleList.add(newNonTerminal)
+              }
+            }
+          }
+          addRule(SimplifiedRule(newRuleList), nonTerminal, targetRules)
+        }
+      }
+    }
+
+    public fun fromGrammar(grammar: Grammar): SimplifiedGrammar {
+      val targetRules = HashMap<NonTerminal, ArrayList<SimplifiedRule>>()
+      for ((nonTerminal, rule) in grammar.rules) {
+        processRule(nonTerminal, rule, targetRules)
+      }
+      return SimplifiedGrammar(targetRules)
+    }
   }
-  
 }
---
+++ src/test/kotlin/GrammarTest.kt
@@ -0,0 +1,23 @@
+import org.junit.Test
+import org.junit.Assert.*
+import grammar.*
+
+class GrammarTest{
+  @Test
+  fun grammarTest(){
+    val a =  NonTerminal("a")
+    val b =  NonTerminal("b")
+    val c =  NonTerminal("c")
+    val A =  Terminal("a")
+    val B =  Terminal("b")
+    val C =  Terminal("c")
+    val grammar = Grammar(
+      a to Sequence(A,b,c),
+      b to Choise(Sequence(B,c), Sequence(c,B), Repeat(B)),
+      c to Sequence(C, Maybe(a))
+    )    
+    println(grammar)
+    val simplifiedGrammar = SimplifiedGrammar.fromGrammar(grammar)
+    println(simplifiedGrammar)
+  }
+}
