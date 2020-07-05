322775a9d6fa790e53759e66ba57ed74e10f73a5
geometriahater <m.v.dyomin@mail.ru>
2020-07-05 13:49:27 +0300

    тест провален, корутины пока не смогли

--- src/test/kotlin/DumbTest.kt
+++ src/test/kotlin/DumbTest.kt
@@ -1,10 +1,22 @@
 import org.junit.Test
 import org.junit.Assert.assertTrue
+import kotlinx.coroutines.*
 
 class DumbTest {
-	@Test
-	fun dumbTest() {
-		val dumbMachiene = DumbMachiene<DumbState>( {it.transition()}, DumbState::isFinite)
-		assertTrue(dumbMachiene.runMachiene(DumbState.STATE1) == DumbState.STATE5)
-	}
+  @Test
+  fun dumbTest() {
+    val time1 : Long = kotlin.system.measureTimeMillis{
+      val dumbMachiene = DumbMachiene<DumbState>( {it.transition()}, DumbState::isFinite)
+      assertTrue(dumbMachiene.runMachiene(DumbState.STATE1) == DumbState.STATE5)
+    }
+
+    val time2 : Long = kotlin.system.measureTimeMillis{
+      val asyncMachiene = AsyncMachiene<DumbState>( {it.transition()}, DumbState::isFinite)
+      assertTrue(asyncMachiene.runMachiene(DumbState.STATE1) == DumbState.STATE5)
+    }
+
+    println(time1)
+    println(time2)
+    assertTrue(time1 > time2)
+  }
 }
