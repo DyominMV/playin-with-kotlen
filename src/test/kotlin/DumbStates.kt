f913408921fb7d383e6ec3b0363a365c379f0f50
geometriahater <m.v.dyomin@mail.ru>
2020-07-05 13:53:16 +0300

    если транзитион дорогой, то корутины выигрывают

--- src/test/kotlin/DumbStates.kt
+++ src/test/kotlin/DumbStates.kt
@@ -1,3 +1,5 @@
+import kotlinx.coroutines.*
+
 enum class DumbState{
   STATE1, STATE2, STATE3, STATE4, STATE5{
     override public fun isFinite(): Boolean = true  
@@ -7,12 +9,15 @@ enum class DumbState{
   };
   public open fun isFinite(): Boolean = false
   
-  public fun transition(): List<DumbState> = when (this){
-    DumbState.STATE1 -> arrayListOf(DumbState.STATE2, DumbState.STATE3)
-    DumbState.STATE2 -> arrayListOf(DumbState.STATE3, DumbState.STATE4)
-    DumbState.STATE3 -> arrayListOf(DumbState.STATE4, DumbState.STATE5)
-    DumbState.STATE4 -> arrayListOf(DumbState.STATE5, DumbState.STATE6)
-    DumbState.STATE5 -> arrayListOf(DumbState.STATE5)
-    DumbState.STATE6 -> arrayListOf(DumbState.STATE6)
+  public fun transition(): List<DumbState> {
+    Thread.sleep(1000)
+    return when (this){
+      DumbState.STATE1 -> arrayListOf(DumbState.STATE2, DumbState.STATE3)
+      DumbState.STATE2 -> arrayListOf(DumbState.STATE3, DumbState.STATE4)
+      DumbState.STATE3 -> arrayListOf(DumbState.STATE4, DumbState.STATE5)
+      DumbState.STATE4 -> arrayListOf(DumbState.STATE5, DumbState.STATE6)
+      DumbState.STATE5 -> arrayListOf(DumbState.STATE5)
+      DumbState.STATE6 -> arrayListOf(DumbState.STATE6)
+    }
   }
 }
