ae2f0fe2ad93a9e61d1e2d7047b7dc12132609b1
geometriahater <m.v.dyomin@mail.ru>
2020-07-06 10:44:31 +0300

    добавил спец. символы. сделал лениво на функциях

--- src/main/kotlin/grammar/Grammar.kt
+++ src/main/kotlin/grammar/Grammar.kt
@@ -4,33 +4,40 @@ sealed class Expression
 
 sealed class Symbol : Expression()
 
-data class NonTerminal(val name: String): Symbol(){
+data class NonTerminal(val name: String) : Symbol() {
   override fun toString(): String = name
 }
 
-data class Terminal(val value: String): Symbol(){
-  override fun toString(): String = "\'"+ value.replace("\'", "\\\'") + "\'"
+data class Terminal(val value: String) : Symbol() {
+  override fun toString(): String = "\'" + value.replace("\'", "\\\'") + "\'"
 }
 
-data class Sequence(val parts: List<Expression>): Expression(){
+data class SpecialNonTerminal(val name: String, val filter: (Char) -> Boolean) : Symbol() {
+  override fun toString(): String = name
+  operator fun not(): SpecialNonTerminal = SpecialNonTerminal(name, { !filter(it) })
+  operator fun plus(other: SpecialNonTerminal): SpecialNonTerminal =
+    SpecialNonTerminal(name + other.name, { filter(it) || other.filter(it) })
+}
+
+data class Sequence(val parts: List<Expression>) : Expression() {
   constructor(vararg exprs: Expression) : this(exprs.toList())
-  override fun toString(): String = parts.fold("", {acc, e -> acc + " " + e})
+  override fun toString(): String = parts.fold("", { acc, e -> acc + " " + e })
 }
 
-data class Choise(val variants: List<Expression>): Expression(){
+data class Choise(val variants: List<Expression>) : Expression() {
   constructor(vararg exprs: Expression) : this(exprs.toList())
-  override fun toString(): String = variants.fold("( ", {acc, e -> acc + e + " | "}).substringBeforeLast("|") + ")"
+  override fun toString(): String = variants.fold("( ", { acc, e -> acc + e + " | " }).substringBeforeLast("|") + ")"
 }
 
-data class Repeat(val repeatable: Expression): Expression(){
+data class Repeat(val repeatable: Expression) : Expression() {
   override fun toString(): String = "{ " + repeatable + " }"
 }
 
-data class Maybe(val possible: Expression): Expression(){  
+data class Maybe(val possible: Expression) : Expression() {
   override fun toString(): String = "[ " + possible + " ]"
 }
 
-data class Grammar(val rules: Map<NonTerminal, Expression>){
-  constructor(vararg pairs: Pair<NonTerminal, Expression>) : this(pairs.associateBy({it.first}, {it.second})) 
-  override fun toString(): String = rules.entries.fold("", {acc, entry -> acc + entry.key + " = " + entry.value + "\n"})
+data class Grammar(val rules: Map<NonTerminal, Expression>) {
+  constructor(vararg pairs: Pair<NonTerminal, Expression>) : this(pairs.associateBy({ it.first }, { it.second }))
+  override fun toString(): String = rules.entries.fold("", { acc, entry -> acc + entry.key + " = " + entry.value + "\n" })
 }
