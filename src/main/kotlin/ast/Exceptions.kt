6b75ee923fc42b4877c01df21bf0dfb612fad47f
geometriahater <m.v.dyomin@mail.ru>
2020-07-13 01:35:06 +0300

    no xceptions

--- src/main/kotlin/ast/Exceptions.kt
+++
@@ -1,7 +0,0 @@
-package ast
-
-class InvalidForkException(fork: Fork) : 
-  Exception("""
-    fork of nonTerminal ${fork.nonTerminal} with rule ${fork.rule} must contain 
-    ${fork.rule.symbols.size} children, but contains only ${fork.children.size} children
-  """)
