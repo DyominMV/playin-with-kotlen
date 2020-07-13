300367399590e97859af290751623b993efd2ae3
geometriahater <m.v.dyomin@mail.ru>
2020-07-13 06:01:28 +0300

    main is still not ignored

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,3 @@
 fun main(args: Array<String>) {
-  if ((null as Boolean?) is Boolean?) println("Okay") else println("not okay")
-  if ((null as Any) is Boolean) println("Okay") else println("not okay")
+  println("hello world" + args.size)
 }
