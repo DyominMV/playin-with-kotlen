4661ec8a3e64b00232ee60b985954f73608bea76
geometriahater <m.v.dyomin@mail.ru>
2020-07-13 05:53:38 +0300

    main...

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,4 @@
-import grammar.*
-import kotlinx.coroutines.*
-
-class Main {
-  companion object {
-    @JvmStatic public fun main(args: Array<String>) {
-      val a = NonTerminal("a")
-      val b = NonTerminal("b")
-      val c = NonTerminal("c")
-      val A = Terminal("a")
-      val B = Terminal("b")
-      val C = Terminal("c")
-      val grammar = Grammar(
-        a to Sequence(A, b, c),
-        b to Choise(Sequence(B, c), Sequence(c, B), Repeat(B)),
-        c to Sequence(C, Maybe(a))
-      )
-      println(grammar)
-      val simplifiedGrammar = SimplifiedGrammar.fromGrammar(grammar)
-      println(simplifiedGrammar)
-    }
-  }
+fun main(args: Array<String>) {
+  if ((null as Boolean?) is Boolean?) println("Okay") else println("not okay")
+  if ((null as Any) is Boolean) println("Okay") else println("not okay")
 }
