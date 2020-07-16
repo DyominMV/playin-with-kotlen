104974596f54715e71a0aaab771e0d5523424745
geometriahater <m.v.dyomin@mail.ru>
2020-07-16 16:25:21 +0300

    parsing exceptions

--- src/main/kotlin/formallang/grammar/Exceptions.kt
+++ src/main/kotlin/formallang/grammar/Exceptions.kt
@@ -2,3 +2,9 @@ package formallang.grammar
 
 class WrongTerminalException() : 
   Exception("Empty terminals are not allowed!")
+
+class NoRuleFoundException(nonTerminal : NonTerminal):
+  Exception("No rule found for non-terminal called $nonTerminal")
+
+class LeftRecursionException(nonTerminal : NonTerminal, rule: SimplifiedRule):
+  Exception("Left recursion found when checking rule:\n\t$nonTerminal = $rule")
