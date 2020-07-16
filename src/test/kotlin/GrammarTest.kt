626903dddd36812f7b46135e81b1a9d3444fc31a
geometriahater <m.v.dyomin@mail.ru>
2020-07-17 02:05:28 +0300

    added useful test

--- src/test/kotlin/GrammarTest.kt
+++ src/test/kotlin/GrammarTest.kt
@@ -1,25 +1,26 @@
+package formallang.grammar
+
 import org.junit.Test
 import org.junit.Assert.*
-import formallang.grammar.*
 
 class GrammarTest{
+  val a =  NonTerminal("a")
+  val b =  NonTerminal("b")
+  val c =  NonTerminal("c")
+  val A =  Terminal("a")
+  val B =  Terminal("b")
+  val C =  Terminal("c")
+  val grammar = Grammar(
+    a to Sequence(A,b,c),
+    b to Choise(Sequence(B,c), Sequence(c,B), Repeat(B)),
+    c to Sequence(C, Maybe(a))
+  )
+  val simplifiedGrammar = SimplifiedGrammar.fromGrammar(grammar)
+
   @Test
   fun grammarTest(){
-    val a =  NonTerminal("a")
-    val b =  NonTerminal("b")
-    val c =  NonTerminal("c")
-    val A =  Terminal("a")
-    val B =  Terminal("b")
-    val C =  Terminal("c")
-    val grammar = Grammar(
-      a to Sequence(A,b,c),
-      b to Choise(Sequence(B,c), Sequence(c,B), Repeat(B)),
-      c to Sequence(C, Maybe(a))
-    )    
-    println(grammar)
-    println( kotlin.system.measureTimeMillis {
-      val simplifiedGrammar = SimplifiedGrammar.fromGrammar(grammar)
-      println(simplifiedGrammar)
-    })
+    println(simplifiedGrammar)
+    println(simplifiedGrammar.getPossibleRules(b, 'b'))
+
   }
 }
