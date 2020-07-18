1f124d8c5162510c954d95fa59fad6f7f5cdd100
geometriahater <m.v.dyomin@mail.ru>
2020-07-18 18:53:04 +0300

    InputStreamReader -> Reader

--- src/main/kotlin/formallang/parser/Parser.kt
+++ src/main/kotlin/formallang/parser/Parser.kt
@@ -1,10 +1,11 @@
 package formallang.parser
 
+import forkable.*
 import formallang.ast.*
 import formallang.grammar.*
 import formallang.metainf.*
 import automata.*
-import java.io.InputStreamReader
+import java.io.Reader
 
 class Parser private constructor(
   stateMachieneFactory: StateMachieneFactory<ParsingState>,
@@ -49,12 +50,22 @@ class Parser private constructor(
     procList.addAll(processorsList)
   }
 
-  private fun justParse(reader: InputStreamReader): Ast {
-    TODO("parse the text")
+  private fun justParse(reader: Reader): Ast? {
+    val state = stateMachiene.runMachiene(RegularState(grammar, ForkableStream({ 
+      val result = reader.read() 
+      if (-1 == result) null else result.toChar()
+    }), mainSymbol))
+    return when(state?.getType()){
+      null -> null
+      State.StateType.FAIL -> null
+      State.StateType.UNFINISHED -> throw IllegalStateException("Cannot finish with unfinished state")
+      State.StateType.SUCCESS -> (state as SuccessState).getAst()
+    }
   }
 
-  public fun parse(reader: InputStreamReader): Ast {
-    return processors.fold(justParse(reader)) {
+  public fun parse(reader: Reader): Ast? {
+    val initialTree = justParse(reader) ?: return null
+    return processors.fold(initialTree) {
       acc, nextProc -> nextProc.process(acc)
     }
   }
